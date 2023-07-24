fac605b63d32a2626fa7c0a410d69ed11507edf9
苏杰豪 <Megasu@qq.com>
2023-07-24 20:58:11 +0800

    feat: 安装 sass 和样式初始化

--- app.vue
+++ app.vue
@@ -2,3 +2,8 @@
   <!-- Nuxt 页面容器，相当于 Vue 的 router-view -->
   <NuxtPage />
 </template>
+
+<style lang="scss">
+@import './assets/styles/base.scss';
+@import './assets/styles/iconfont.scss';
+</style>
