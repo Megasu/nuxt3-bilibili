45aeff3b4f449a9334817a2a98b764a61fcdc6dc
苏杰豪 <Megasu@qq.com>
2023-07-25 16:15:37 +0800

    chore: 安装 @vueuse/nuxt

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   // 应用模块
-  modules: ['@vant/nuxt'],
+  modules: ['@vant/nuxt', '@vueuse/nuxt'],
   // 调试工具
   devtools: { enabled: true },
 })
