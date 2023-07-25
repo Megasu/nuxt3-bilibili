d86c7bfc6032edf8a0d0067770962bd5fe88d88c
苏杰豪 <Megasu@qq.com>
2023-07-25 09:33:39 +0800

    feat: 安装vant组件库

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  // 应用模块
+  modules: ['@vant/nuxt'],
+  // 调试工具
+  devtools: { enabled: true },
 })
