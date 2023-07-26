5899a1af4f15167667a012fbd76c98b1dcc77e36
苏杰豪 <Megasu@qq.com>
2023-07-26 10:36:29 +0800

    feat: 添加 px to vw 适配

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,4 +4,12 @@ export default defineNuxtConfig({
   modules: ['@vant/nuxt', '@vueuse/nuxt'],
   // 调试工具
   devtools: { enabled: true },
+  // 移动端适配
+  postcss: {
+    plugins: {
+      'postcss-px-to-viewport': {
+        viewportWidth: 375,
+      },
+    },
+  },
 })
